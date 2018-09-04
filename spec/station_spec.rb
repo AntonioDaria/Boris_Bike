ef07aa61bc0bf5e35d1030d65f13836bbaf0f6a4
Antonio <antoniodaria@msn.com>
2018-09-04 21:33:05 +0100

    Adding the release_bike method to pass the unit test

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,8 +1,8 @@
 require './lib/docking'
 
-
   describe DockingStation do
-    it 'returns "release bike" working is true ' do
-      expect(working(true)).to eq 'release bike'
-    end
+    # Add a test that expects DockingStation instances to respond_to the method release_bike
+    it 'respond_to release_bike' do
+    expect(subject).to respond_to :release_bike
   end
+end
